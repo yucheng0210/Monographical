19ede431d2c8f7973bdf13f803dbd3c5eb3073b1
洋芋片 <yuchengyang9487@gmail.com>
2024-02-06 22:30:38 +0800

    開門動作微調&&關卡設計
    開門角色方向以及位置調整，和地牢關卡設計。

--- Assets/MyAssets/Dungeon/Script/OpenDoor.cs
+++ Assets/MyAssets/Dungeon/Script/OpenDoor.cs
@@ -12,14 +12,17 @@ public class OpenDoor : MonoBehaviour
     private float openTime;
     [SerializeField]
     private float openAngle;
+    [SerializeField]
+    private Transform lookTrans;
     private bool isOpen;
     private void OnTriggerStay(Collider collider)
     {
         if (collider.CompareTag("Player") && Input.GetKeyDown(KeyCode.E) && !isOpen)
         {
             isOpen = true;
+            GameManager.Instance.PlayerTrans.LookAt(lookTrans);
+            GameManager.Instance.PlayerTrans.position = lookTrans.position;
             GameManager.Instance.PlayerAni.SetTrigger("isOpenDoor");
-            GameManager.Instance.PlayerTrans.LookAt(transform);
             leftDoorTrans.DOLocalRotate(new Vector3(0, openAngle, 0), openTime);
             rightDoorTrans.DOLocalRotate(new Vector3(0, -openAngle, 0), openTime);
         }
